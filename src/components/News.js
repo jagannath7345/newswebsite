b0046fac2964d389c2802e17c6400d1657f33949
Jagannath <guptajn7345@gmail.com>
2022-08-31 09:52:24 +0530

    my frist news website

---
+++ src/components/News.js
@@ -0,0 +1,114 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+export class News extends Component {
+  static defaultProps = {
+    country: "in",
+    pageSize: 6,
+    category: "general",
+  };
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  };
+  capitalizerFirstLeter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+      totalResults:0
+    };
+    document.title = `${this.capitalizerFirstLeter(
+      this.props.category
+    )} - LatestNews`;
+  }
+
+  async updateNews() {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b0840087e6a34a778d6f6c39300ba4cc&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedata = await data.json();
+    this.setState({
+      articles: parsedata.articles,
+      totalResults: parsedata.totalResults,
+      loading: false,
+    });
+  }
+
+  async componentDidMount() {
+    this.updateNews();
+  }
+
+  // handlePrevClick = async () => {
+  //   this.setState({ page: this.state.page - 1 });
+  //   this.updateNews();
+  // };
+
+  // handleNextClick = async () => {
+  //   this.setState({ page: this.state.page + 1 });
+  //   this.updateNews();
+  // };
+
+  fetchMoreData = async () => {
+      this.setState({ page: this.page + 1});
+      const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b0840087e6a34a778d6f6c39300ba4cc&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedata = await data.json();
+    this.setState({
+      articles: this.state.articles.concat(parsedata.articles),
+      totalResults: parsedata.totalResults,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <h1 className="text-center" style={{ margin: "35px 0px" }}>
+          LetestNews - Top {this.capitalizerFirstLeter(this.props.category)}{" "}
+          headlines
+        </h1>
+        {this.state.loading && <Spinner />}
+
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner />}
+        >
+          <div className="container">
+          <div className="row">
+            {this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={element.title}
+                    description={element.description}
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                    author={element.author}
+                    date={element.publishedAt}
+                    source={element.source.name}
+                  />
+                </div>
+              );
+            })}
+          </div>
+          </div>
+        </InfiniteScroll>
+
+        
+      </>
+    );
+  }
+}
+
+export default News;
---
+++ src/components/NewsItem.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    let {title, description, imageUrl,newsUrl,author,date,source,} = this.props;
+    return (
+        <div className='my-3'>
+        <div className="card">
+        <span className=" position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%',zIndex:'1'}}>{source}</span>
+        <img className="card-img-top" src={!imageUrl?"https://images.newindianexpress.com/uploads/user/imagelibrary/2022/8/25/w600X390/AP19100474348599.jpg":imageUrl} alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <p className="card-text"><small className="text-muted">By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
+          <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
+        </div>
+      </div>
+      </div>
+    )
+  }
+}
+
+export default NewsItem
